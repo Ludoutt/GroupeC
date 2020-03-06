9698a3334eca237f86807ec683bd4e497f25a6a3
Tom Reversez <tom.reversez@gmail.com>
2020-03-06 11:58:41 +0100

    Feat - Liste des taches d'un projet et ajouter une tache à un projet

--- src/Form/TaskFormType.php
+++ src/Form/TaskFormType.php
@@ -2,25 +2,30 @@
 
 namespace App\Form;
 
-use App\Entity\Project;
+use App\Entity\Task;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ProjectType extends AbstractType
+class TaskFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('title')
             ->add('resume')
+            ->add('priority')
+            ->add('status')
+            ->add('tags')
+            ->add('ajouter', SubmitType::class)
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Project::class,
+            'data_class' => Task::class,
         ]);
     }
 }
